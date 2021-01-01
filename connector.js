55263c3f17f2e06437ce72787056670a88b70da9
pawarss1 <pawarss_1@rknec.edu>
2021-01-01 15:49:36 +0530

    Node Server Setup

---
+++ connector.js
@@ -0,0 +1,17 @@
+//const mongoURI = "mongodb://localhost:27017" + "/todoList"
+const mongoURI = "mongodb+srv://cris:cris@todo-cluster.baijx.mongodb.net/TodoApp?retryWrites=true&w=majority"
+//mongodb
+
+let mongoose = require('mongoose');
+const { todoSchema, userSchema } = require('./schema')
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log("connection established with mongodb server online"); })
+    .catch(err => {
+        console.log("error while connection", err)
+    });
+const userDb = mongoose.model('Users', userSchema)
+const todoTasksDb = mongoose.model('TodoTasks', todoSchema)
+
+exports.todoTasksDb = todoTasksDb;
+exports.userDb = userDb;
